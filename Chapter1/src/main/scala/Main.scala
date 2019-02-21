1d9c9825e4c4f6f442acb055262dca1e5bb03166
Sergii Zhuravskyi <SZhuravskyi@navinet.net>
2019-02-21 18:15:45 +0000

    push

--- Chapter1/src/main/scala/Main.scala
+++ Chapter1/src/main/scala/Main.scala
@@ -4,6 +4,8 @@ object Main {
     println("Hello, THOMAS")
     val cafe = new Cafe()
     println(cafe.printItSelf())
+
+    println(cafe.formatAbs(-42))
   }
 }
 
@@ -11,5 +13,14 @@ class Cafe {
   var myVar: String = "From Cafee"
   def printItSelf(): String = {
     myVar
+  }
+
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
+
+  def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
   }
 }
