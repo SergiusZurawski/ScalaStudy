e33d8e6c29ba82b3e0801cffb204e0ba6e4a84f6
Sergius Home Stationary <sergii.zhuravskyi@gmail.com>
2019-02-23 21:49:52 +0000

    fibonachi

--- Chapter1/src/main/scala/Main.scala
+++ Chapter1/src/main/scala/Main.scala
@@ -2,10 +2,12 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
     println("Hello, THOMAS")
-    val cafe = new Cafe()
-    println(cafe.printItSelf())
+    val fibonacci = new Fibonacci()
+    //println(fibonacci.fiv(3))
+    //println(fibonacci.fibNotTail(10))
+    //println(fibonacci.fib(18))
+    println(fibonacci.fibNoIndex(18))
 
-    println(cafe.formatAbs(-42))
   }
 }
 
@@ -24,3 +26,79 @@ class Cafe {
     msg.format(x, abs(x))
   }
 }
+
+class Fibonacci {
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, index: Int, current: Int , previous: Int): Int = {
+      if(index <= 0) {
+        loop(n, index+1, 0, 0)
+      }
+      else if (index == 1){
+        loop(n, index+1, 1+0, 1)
+      }
+      else if (index >= n)
+        current
+      else {
+        println("loop")
+        println("n: " + n)
+        println("current: " + current)
+        println("previous: " + previous)
+        loop(n, index+1, current+previous, current)
+      }
+    }
+
+    loop(n, 1, 0, 0)
+  }
+
+  def fibNoIndex(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, current: Int , previous: Int): Int = {
+      if(n <= 0) {
+        current
+      }
+      else {
+        loop(n-1,  current+previous, current)
+      }
+    }
+
+    loop(n, 1, 0)
+  }
+
+
+  def fibNotTail(n: Int): Int = {
+    def loop(n: Int): Int = {
+      if(n <= 0) {
+        0
+      }
+      else if(n == 1) {
+        1
+      }
+      else {
+        println("loop")
+        println("n: " + n)
+        println("result: ")
+        loop(n - 1) + loop(n - 2)
+      }
+    }
+    loop(n)
+  }
+
+}
+/*
+*
+*  if(n <= 1) {
+        println("n<=0")
+        println(result)
+        result
+      }
+      else if(n == 2) {
+        println("n==1")
+        result + 1
+      }
+      else {
+        println("loop")
+        println("n: " + n)
+        println("result: " + result)
+        loop(n-1,result + n)
+      }*/
