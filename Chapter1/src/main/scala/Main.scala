7bc12a968f736a4e96a18789c6bb6bd1553bd55b
Sergius Home Stationary <sergii.zhuravskyi@gmail.com>
2019-02-27 18:53:05 +0000

    int solution

--- Chapter1/src/main/scala/Main.scala
+++ Chapter1/src/main/scala/Main.scala
@@ -3,10 +3,14 @@ object Main {
     println("Hello, world!")
     println("Hello, THOMAS")
     val fibonacci = new Fibonacci()
+    val poli = new PolimopicFuncition()
     //println(fibonacci.fiv(3))
     //println(fibonacci.fibNotTail(10))
     //println(fibonacci.fib(18))
-    println(fibonacci.fibNoIndex(18))
+//    println(fibonacci.fibNoIndex(18))
+    println(poli.isSortedInt(Array(1, 2, 3)))
+    println(poli.isSortedInt(Array(2, 2, 3)))
+    println(poli.isSortedInt(Array(3, 2, 3)))
 
   }
 }
@@ -29,28 +33,46 @@ class Cafe {
 
 class PolimopicFuncition
 {
-  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean ={
-    //as.
-  }
-
-  def isSortedInt(as: Array[Int], ordered: (Int, Int) => Boolean): Boolean ={
-
-  }
+//  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean ={
+//    //as.
+//  }
+//
+//  def isSortedInt(as: Array[Int], ordered: (Int, Int) => Boolean): Boolean ={
+//
+//  }
 
   def isSortedInt(as: Array[Int]): Boolean ={
+    def loop(as: Array[Int], index: Int, previous: Int ): Boolean = {
 
-  }
+      if(as.length <= index)
+        return true
 
-  def isSortedIntNotRecursive(as: Array[Int]): Boolean ={
-    var item1: Int = 0
-    var item2: Int = 0
-    var index = 0;
-    for(item <-as)
-    {
-      index += 1;
-      if()
+      val current = as(index)
+      if(current < previous)
+        return false
+
+      loop(as, index+1, current)
+
     }
+
+    if(as.length < 1) {
+      false
+    }
+
+
+    return loop(as, 1, as(0))
   }
+
+//  def isSortedIntNotRecursive(as: Array[Int]): Boolean ={
+//    var item1: Int = 0
+//    var item2: Int = 0
+//    var index = 0;
+//    for(item <-as)
+//    {
+//      index += 1;
+//      if()
+//    }
+//  }
 
 }
 
